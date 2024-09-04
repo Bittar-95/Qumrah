1eab036edc6fb785490b1e713b82020890e4b241
Mohammad Albitar <m.albitar@desaisiv.com>
2024-09-05 00:08:11 +0300

    update

---
+++ Qumrah.Web/Controllers/UserProfile.cs
@@ -0,0 +1,54 @@
+﻿using aspnetcore.ntier.BLL.Services.IServices;
+using aspnetcore.ntier.DTO.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Qumrah.Web.Models.UserProfile;
+
+namespace Qumrah.Web.Controllers
+{
+    [Authorize]
+    public class UserProfile : Controller
+    {
+        private readonly IUserService _userService;
+
+        public UserProfile(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public async Task<IActionResult> EditProfile()
+        {
+            var user = await _userService.GetAsync(User.Identity.Name);
+            var editProfileVM = new EditProfileVM
+            {
+                Description = user.Description,
+                TwitterLink = user.TwitterLink,
+                FBLink = user.FBLink,
+                FirstName = user.FirstName,
+                InstagramLink = user.InstagramLink,
+                LastName = user.LastName,
+            };
+            return View(editProfileVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditProfile(EditProfileVM model)
+        {
+            var user = await _userService.GetAsync(User.Identity.Name);
+            var imagePath = "test";
+            await _userService.EditAsync(new EditUserDto
+            {
+                Id = user.Id,
+                Description = model.Description,
+                FBLink = model.FBLink,
+                ImagePath = imagePath,
+                InstagramLink = model.InstagramLink,
+                TwitterLink = model.TwitterLink,
+                LastName = model.LastName,
+                FirstName = model.FirstName,
+            });
+            return View();
+        }
+
+    }
+}
---
+++ Qumrah.Web/Models/UserProfile/EditProfileVM.cs
@@ -0,0 +1,14 @@
+﻿namespace Qumrah.Web.Models.UserProfile
+{
+    public class EditProfileVM
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? FBLink { get; set; }
+        public string? TwitterLink { get; set; }
+        public string? InstagramLink { get; set; }
+        public string? Description { get; set; }
+        public IFormFile? ImageProfile { get; set; }
+    }
+}
