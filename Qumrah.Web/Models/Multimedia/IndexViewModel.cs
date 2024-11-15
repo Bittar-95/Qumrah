6613deb950c7864aad3bc88832d1b2ce4ed1381a
Mohammad Albitar <m.albitar@desaisiv.com>
2024-11-15 17:45:28 +0300

    fix home page issues

--- Qumrah.Web/Models/Multimedia/IndexViewModel.cs
+++ Qumrah.Web/Models/Multimedia/IndexViewModel.cs
@@ -4,6 +4,7 @@ namespace Qumrah.Web.Models.Multimedia
 {
     public class IndexViewModel
     {
+        public FilterMultimediaViewModel Filter { get; set; }
         public List<MultimediaVM> Multimedia { get; set; }
         public List<string> Locations { get { return Multimedia?.Select(l => l.Location).Where(t => !string.IsNullOrEmpty(t)).ToList(); } }
     }
